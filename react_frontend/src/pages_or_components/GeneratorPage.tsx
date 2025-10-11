a4715dbb3c272683cddba6d168d692ac2e308001
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-10-11 15:36:03 -0400

    please encode

--- react_frontend/src/pages_or_components/GeneratorPage.tsx
+++ react_frontend/src/pages_or_components/GeneratorPage.tsx
@@ -92,7 +92,7 @@ export default function GeneratorPage() {
     }
     
     // runs getRecs implicitly
-    setResourceForQuery({type: linkType, sanitizedLink: encodeURIComponent(sanitizedLink)})
+    setResourceForQuery({type: linkType, sanitizedLink: sanitizedLink})
   }
 
   const { data, error, isLoading } = getRecs(resourceForQuery)
