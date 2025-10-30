b60e2642d89519b5ac6b4a4607fb004c6fb3c588
huynhli <liam.huynh@mail.utoronto.ca>
2025-10-30 18:08:00 -0400

    some changes

--- go_backend/go.mod
+++ go_backend/go.mod
@@ -2,7 +2,16 @@ module go_backend
 
 go 1.23.4
 
-require github.com/gofiber/fiber/v2 v2.52.6
+require (
+	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
---
+++ go_backend/integration/handlers_test.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"go_backend/config"
+	"go_backend/handlers"
+	"go_backend/routes"
+	"io"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetLastFMTrackRoute(t *testing.T) {
+	config.LoadConfig()
+	app := fiber.New()
+	app.Get("/test", routes.GetLastFMTrack)
+
+	req := httptest.NewRequest("GET", "/test?link=https://open.spotify.com/artist/6vWDO969PvNqNYHIOW5v0m?si=i8pS1ntxTF-tCTV1rItksw", nil)
+	resp, err := app.Test(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+
+	body, _ := io.ReadAll(resp.Body)
+
+	// Check that the JSON contains the expected fields
+	assert.Contains(t, string(body), `"artist"`)
+	// assert.Contains(t, string(body), `"track"`)
+	// assert.Contains(t, string(body), `"album"`)
+}
+
+func TestSpotifyAndLastFMHandlers(t *testing.T) {
+	// TODO group tests
+}
+
+// TODO mockable tests
+
+func TestSpotifyInNameOutArtist(t *testing.T) {
+	config.LoadConfig()
+	resp, err := handlers.SpotifyInNameOut("https://open.spotify.com/artist/6vWDO969PvNqNYHIOW5v0m?si=i8pS1ntxTF-tCTV1rItksw")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp.ArtistName)
+	assert.Equal(t, "Beyoncé", resp.ArtistName)
+}
+
+func TestSpotifyInNameOutTrack(t *testing.T) {
+	config.LoadConfig()
+	resp, err := handlers.SpotifyInNameOut("https://open.spotify.com/track/0TwBtDAWpkpM3srywFVOV5")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp.ArtistName)
+	assert.NotEmpty(t, resp.TrackName)
+	assert.NotEmpty(t, resp.AlbumName)
+	assert.Equal(t, "Beyoncé", resp.ArtistName)
+	// assert.Equal(t, []string{"Beyoncé", "JAY-Z"}, resp.ArtistName)
+	assert.Equal(t, "Crazy In Love (feat. JAY-Z)", *resp.TrackName)
+	assert.Equal(t, "Dangerously In Love", *resp.AlbumName)
+}
+
+func TestSpotifyInNameOutAlbum(t *testing.T) {
+	config.LoadConfig()
+	resp, err := handlers.SpotifyInNameOut("https://open.spotify.com/album/75sBENwOba7T6buiOJsAZs?si=3xw4VPFAQ3mbXHq3EqFtfQ")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp.ArtistName)
+	assert.NotEmpty(t, resp.AlbumName)
+	assert.Equal(t, "Long Live The Empire", resp.ArtistName)
+	assert.Equal(t, "Deathless", *resp.AlbumName)
+}
+
+func TestLastFMSimilarTracksBuiltin(t *testing.T) {
+	config.LoadConfig()
+	resp, err := handlers.GetLastFMSimilarTracksBuiltin("believe", "cher")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "Cher", resp[0].ArtistName)
+	assert.Equal(t, "Strong Enough", resp[0].RecName)
+}
+
+func TestLastFMSimilarArtistsBuiltin(t *testing.T) {
+	config.LoadConfig()
+	resp, err := handlers.GetLastFMSimilarArtistsBuiltin("cher")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "Sonny & Cher", resp[0])
+}
+
+func TestLastFMTracksByTag(t *testing.T) {
+	config.LoadConfig()
+	tag := []string{"disco"}
+	resp, err := handlers.GetLastFMTracksByTag(tag)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "Dancing Queen", resp[tag[0]][0].RecName)
+	assert.Equal(t, "ABBA", resp[tag[0]][0].ArtistName)
+}
+
+func TestLastFMAlbumByTag(t *testing.T) {
+	config.LoadConfig()
+	tag := []string{"disco"}
+	resp, err := handlers.GetLastFMAlbumsByTag(tag)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "Bad Girls", resp[tag[0]][0].RecName)
+	assert.Equal(t, "Donna Summer", resp[tag[0]][0].ArtistName)
+}
+
+func TestLastFMArtistByTag(t *testing.T) {
+	config.LoadConfig()
+	tag := []string{"disco"}
+	resp, err := handlers.GetLastFMArtistsByTag(tag)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "ABBA", resp[tag[0]][0])
+}
+
+func TestLastFMTrackTags(t *testing.T) {
+	config.LoadConfig()
+	trackTemp := "Paranoid Android"
+
+	testResp := handlers.SpotifyResponseObj{
+		ArtistName: "Radiohead",
+		TrackName:  &trackTemp,
+	}
+	tags, err := handlers.GetLastFMTags(testResp)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, tags)
+	assert.Equal(t, "alternative rock", tags[0])
+	assert.Equal(t, "alternative", tags[1])
+	assert.Equal(t, "rock", tags[2])
+}
+
+func TestLastFMAlbumTags(t *testing.T) {
+	config.LoadConfig()
+	albumTemp := "The Bends"
+
+	testResp := handlers.SpotifyResponseObj{
+		ArtistName: "Radiohead",
+		AlbumName:  &albumTemp,
+	}
+	tags, err := handlers.GetLastFMTags(testResp)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, tags)
+	assert.Equal(t, "alternative rock", tags[0])
+	assert.Equal(t, "1995", tags[1])
+	assert.Equal(t, "rock", tags[2])
+}
+
+func TestLastFMArtistTags(t *testing.T) {
+	config.LoadConfig()
+	testResp := handlers.SpotifyResponseObj{
+		ArtistName: "Cher",
+	}
+	tags, err := handlers.GetLastFMTags(testResp)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, tags)
+	assert.Equal(t, "pop", tags[0])
+	assert.Equal(t, "female vocalists", tags[1])
+	assert.Equal(t, "80s", tags[2])
+}
+
+// func TestSpotifyInNameOutAlbum(t *testing.T) {
+// 	config.LoadConfig()
+// 	app := fiber.New()
+// 	app.Get("/test", handlers.InToOut)
+
+// 	req := httptest.NewRequest("GET", "/test?link=https://open.spotify.com/album/6BzxX6zkDsYKFJ04ziU5xQ?si=cchby_lyTES2MnA1L2kleg", nil)
+// 	resp, err := app.Test(req)
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, 200, resp.StatusCode)
+
+// 	body, _ := io.ReadAll(resp.Body)
+
+// 	// Check that the JSON contains the expected fields
+// 	assert.Contains(t, string(body), `"artist"`)
+// 	// assert.Contains(t, string(body), `"track"`)
+// 	assert.Contains(t, string(body), `"album"`)
+// }
+
+// func TestSpotifyInNameOutTrack(t *testing.T) {
+// 	config.LoadConfig()
+// 	app := fiber.New()
+// 	app.Get("/test", handlers.InToOut)
+
+// 	req := httptest.NewRequest("GET", "/test?link=https://open.spotify.com/track/0TwBtDAWpkpM3srywFVOV5?si=2c1aa537c2fe4340", nil)
+// 	resp, err := app.Test(req)
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, 200, resp.StatusCode)
+
+// 	body, _ := io.ReadAll(resp.Body)
+
+// 	// Check that the JSON contains the expected fields
+// 	assert.Contains(t, string(body), `"artist"`)
+// 	assert.Contains(t, string(body), `"track"`)
+// 	assert.Contains(t, string(body), `"album"`)
+// }
+
+// TODO more tests
